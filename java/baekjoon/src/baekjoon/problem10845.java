5ea3cb02379ea09e9f83bd7a599b9448a806180e
newoni <newoni_@naver.com>
2021-02-28 23:21:04 +0900

    problem number 10845 modified

--- java/baekjoon/src/baekjoon/problem10845.java
+++ java/baekjoon/src/baekjoon/problem10845.java
@@ -1,48 +1,67 @@
+/*
+ * <21.02.28>
+ * made by KH
+ * 
+ * source: https://www.acmicpc.net/problem/10845
+ * reference: https://coding-factory.tistory.com/251
+ * reference: https://code0xff.tistory.com/10
+ */
+
 package baekjoon;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Queue{
 	ArrayList<Integer> queueList = new ArrayList<Integer>();
 	
+//	<21.02.28> BufferedWriter È°¿ë
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
 	public void push(int inputNumber) {
 		queueList.add(inputNumber);
 	}
 	
-	public void pop() {
+	public int pop()  {
 		if((queueList.size()) ==0) {
-			System.out.println(-1);
+			return -1;
 		}else {
-			System.out.println(queueList.get(0));
+			int res = queueList.get(0);
 			queueList.remove(0);
+			
+			return res; 
 		}
 	}
 	
-	public void size() {
-		System.out.println(queueList.size());
+	public int size() {
+		return queueList.size();
 	}
 	
-	public void empty() {
+	public int empty(){
 		if(queueList.size()==0) {
-			System.out.println(1);
+			return 1;
 		}else {
-			System.out.println(0);
+			return 0;
 		}
 	}
 	
-	public void front() {
+	public int front() {
 		if((queueList.size()) ==0) {
-			System.out.println(-1);
+			return -1;
 		}else {
-			System.out.println(queueList.get(0));
+			return queueList.get(0);
 		}
 	}
 	
-	public void back() {
+	public int back(){
 		if((queueList.size()) ==0) {
-			System.out.println(-1);
+			return -1;
 		}else {
-			System.out.println(queueList.get(queueList.size()-1));
+			return queueList.get(queueList.size()-1);
 		}
 	}
 	
@@ -50,30 +69,47 @@ class Queue{
 
 public class problem10845 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+//		Scanner sc = new Scanner(System.in);
+//		int inputNumber = sc.nextInt();
 		
-		Scanner sc = new Scanner(System.in);
-		int inputNumber = sc.nextInt();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int inputNumber = Integer.parseInt(bf.readLine());
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		Queue queueTest = new Queue();
 		
+		
 		for(int i=0; i<inputNumber; i++) {
-			String command = sc.next();
+//			String command = sc.next();
+			String command = bf.readLine();
 			
-			if(command.equals("push")) {
-				int number = sc.nextInt();
-				queueTest.push(number);
-			}else if(command.equals("pop")) {
-				queueTest.pop();
+			
+			if(command.equals("pop")) {
+				bw.write(Integer.toString(queueTest.pop()));
+				bw.newLine();
+				bw.flush();
 			}else if(command.equals("size")) {
-				queueTest.size();
+				bw.write(Integer.toString(queueTest.size()));
+				bw.newLine();
+				bw.flush();
 			}else if(command.equals("empty")) {
-				queueTest.empty();
+				bw.write(Integer.toString(queueTest.empty()));
+				bw.newLine();
+				bw.flush();
 			}else if(command.equals("front")) {
-				queueTest.front();
+				bw.write(Integer.toString(queueTest.front()));
+				bw.newLine();
+				bw.flush();
 			}else if(command.equals("back")) {
-				queueTest.back();
+				bw.write(Integer.toString(queueTest.back()));
+				bw.newLine();
+				bw.flush();
+			}else { // push
+				String[] array = command.split(" ");
+				queueTest.push(Integer.parseInt(array[1]));
 			}
 		}
 		
