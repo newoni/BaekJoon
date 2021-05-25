5898bb2d2a7485097abe92f5ec0d0881d9678c56
newoni <newoni_@naver.com>
2021-05-26 00:02:03 +0900

    problem number 10430 with java <21.05.25>

---
+++ java/baekjoon/src/baekjoon/problem10430.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem10430 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int numA = Integer.parseInt(st.nextToken());
+		int numB = Integer.parseInt(st.nextToken());
+		int numC = Integer.parseInt(st.nextToken());
+		
+		System.out.println((numA + numB)%numC);
+		System.out.println(((numA%numC) + (numB%numC))%numC);
+		System.out.println((numA*numB)%numC);
+		System.out.println(((numA%numC)*(numB%numC))%numC);
+	}
+
+}
